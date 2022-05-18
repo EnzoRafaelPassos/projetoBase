131c16760501b05f46f19ded36679149db026f11
EnzoRafaelPassos <enzorafel14@gmail.com>
2022-05-17 22:39:15 -0300

    Media dos alunos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projetoBase';
+  codigoRMA: number | undefined;
+   Nome: string = "";
+   nota1: number = 0;
+   nota2: number = 0;
+   nota3: number = 0;
+   nota4: number = 0;
+   Media: number = 0;
+
+  constructor() {
+   this.Media = this.nota1 + this.nota2 + this.nota3 + this.nota4 /4;
+
+  }
+
+  ngOnInit(): void {
+  }
 }
