d279702ea33439e1c51309b0a30c17e63bb9cdd2
EnzoRafaelPassos <enzorafel14@gmail.com>
2022-05-31 02:48:57 -0300

    Calcular Media

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NgModel } from '@angular/forms';
 
@@ -21,6 +22,7 @@ export class AppComponent implements OnInit {
   nota3: number = 0;
   nota4: number = 0;
   Media = (this.nota1 + this.nota2 + this.nota3 + this.nota4) / 4;
+  data: Date = new Date();
  
 
   calcularMedia() {
